e8a60c91ee814bb59ba8c5af0eeeb74b9fde1d9b
nessnesskbigha <73502224+nessnesskbigha@users.noreply.github.com>
2020-10-27 01:33:27 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+const mongoose   = require('mongoose');
+var bodyParser = require('body-parser');
+var app = express()
+app.set('view engine','ejs');
+
+var routes = require('./routes')
+
+app.use(express.static(__dirname + '/assets/'));
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//C O N F I G U R A T I O N // MONGO-DB
+
+//
+//connecting to mongoDB
+mongoose.connect("mongodb+srv://tizmo:keklord@cluster0.gwthe.gcp.mongodb.net/cc?retryWrites=true&w=majority",{useNewUrlParser : true, useUnifiedTopology: true})
+    .then(console.log("Mongo Connected"))
+    .catch(err => console.log(err));
+
+    
+
+app.use('/',routes);
+
+var port = process.env.PORT || 3000
+
+app.listen(port, () =>{
+    console.log(`listening on ${port}`);
+})
